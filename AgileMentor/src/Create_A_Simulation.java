fbcbfe8ac06a6249df6139296eac85958408105b
rgajab <rgajab@asu.edu>
2023-10-27 01:39:16 -0700

    Changes in Simulation in Progress Page

---
+++ AgileMentor/src/Create_A_Simulation.java
@@ -0,0 +1,429 @@
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.BorderFactory;
+import javax.swing.border.MatteBorder;
+import javax.swing.plaf.basic.BasicComboBoxUI;
+import javax.swing.plaf.basic.BasicComboPopup;
+import javax.swing.plaf.basic.ComboPopup;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.geom.RoundRectangle2D;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Shape;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
+import javax.swing.JTextField;
+import javax.swing.ScrollPaneConstants;
+import java.awt.BasicStroke;
+import java.awt.Color;
+import javax.swing.JComboBox;
+import javax.swing.SpringLayout;
+
+public class Create_A_Simulation
+{
+
+	private JFrame frame;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
+	private SimulationInProgressBackEnd backendcalltoSIPBE = new SimulationInProgressBackEnd();
+
+	
+	
+	public JTextField getTextField_1() {
+		return textField_1;
+	}
+
+	public void setTextField_1(JTextField textField_1) {
+		this.textField_1 = textField_1;
+	}
+
+	public JTextField getTextField_2() {
+		return textField_2;
+	}
+
+	public void setTextField_2(JTextField textField_2) {
+		this.textField_2 = textField_2;
+	}
+
+	public JTextField getTextField_3() {
+		return textField_3;
+	}
+
+	public void setTextField_3(JTextField textField_3) {
+		this.textField_3 = textField_3;
+	}
+
+	public JTextField getTextField_4() {
+		return textField_4;
+	}
+
+	public void setTextField_4(JTextField textField_4) {
+		this.textField_4 = textField_4;
+	}
+
+	public JTextField getTextField_5() {
+		return textField_5;
+	}
+
+	public void setTextField_5(JTextField textField_5) {
+		this.textField_5 = textField_5;
+	}
+
+	public static void main(String[] args)
+	{
+		EventQueue.invokeLater(new Runnable()
+		{
+			public void run()
+			{
+				try
+				{
+					Create_A_Simulation window = new Create_A_Simulation();
+					window.frame.setVisible(true);
+				} catch (Exception e)
+				{
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Create_A_Simulation()
+	{
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize()
+	{
+		frame = new JFrame();
+		frame.setBounds(350, 120, 754, 719); // Size of frame
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new SpringLayout()); // Use SpringLayout
+		frame.getContentPane().setLayout(null);
+		frame.getContentPane().setBackground(Color.decode("#f0ebd8"));
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+		
+		JButton btnNewButton_1 = new JButton("Sign Out");
+		btnNewButton_1.setForeground(Color.RED);
+		btnNewButton_1.setFont(new Font("SansSerif", Font.PLAIN, 13));
+		btnNewButton_1.setOpaque(false); // Transparent color
+		btnNewButton_1.setContentAreaFilled(false); 
+		btnNewButton_1.setBorderPainted(false); // Hide border
+		btnNewButton_1.setFocusPainted(false);
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) { // Need to make link between LoginPage
+				frame.dispose();
+				
+				LoginPage LoginPage = new LoginPage(); // New Frame (NewScreen)
+				LoginPage.setVisible(true);
+				
+			}
+		});
+		btnNewButton_1.setBounds(40, 22, 120, 40);
+		frame.getContentPane().add(btnNewButton_1);
+		
+		JLabel lblNewLabel = new JLabel("Create A Simulation");
+		lblNewLabel.setFont(new Font("SansSerif", Font.BOLD, 32));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(226, 24, 305, 43);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_2 = new JLabel("Session Name");
+		lblNewLabel_2.setFont(new Font("SansSerif", Font.PLAIN, 13));
+		lblNewLabel_2.setBounds(228, 84, 120, 15);
+		frame.getContentPane().add(lblNewLabel_2);
+
+		MatteBorder bottomBorder = BorderFactory.createMatteBorder(0, 0, 2, 0, Color.decode("#3e5c76"));
+		textField_1 = new JTextField();
+		textField_1.setBackground(new Color(0, 0, 0));
+		textField_1.setForeground(new Color(0, 0, 0));
+		textField_1.setOpaque(false); // transparent
+		textField_1.setFont(new Font("SansSerif", Font.PLAIN, 13));
+		textField_1.setBorder(bottomBorder);
+		textField_1.setBounds(228, 109, 295, 25);
+		textField_1.setColumns(10);
+		frame.getContentPane().add(textField_1);
+		
+		JLabel lblNewLabel_3 = new JLabel("Set Configuration:");
+		lblNewLabel_3.setFont(new Font("SansSerif", Font.PLAIN, 20));
+		lblNewLabel_3.setBounds(150, 182, 162, 34);
+		frame.getContentPane().add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_4 = new JLabel("or");
+		lblNewLabel_4.setFont(new Font("SansSerif", Font.PLAIN, 13));
+		lblNewLabel_4.setBounds(380, 190, 13, 17);
+		frame.getContentPane().add(lblNewLabel_4);
+		
+		RoundButton btnNewButton_2 = new RoundButton("Import Configuration");
+		btnNewButton_2.setForeground(Color.WHITE);
+		btnNewButton_2.setFont(new Font("SansSerif", Font.PLAIN, 13));
+		btnNewButton_2.setBackground(Color.decode("#3e5c76"));
+		btnNewButton_2.setFocusPainted(false);
+		btnNewButton_2.setBorderPainted(false);
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_2.setBounds(460, 176, 162, 45);
+		frame.getContentPane().add(btnNewButton_2);
+		
+		JLabel lblNewLabel_5 = new JLabel("Number of Sprints");
+		lblNewLabel_5.setFont(new Font("SansSerif", Font.PLAIN, 13));
+		lblNewLabel_5.setBounds(219, 246, 120, 18);
+		frame.getContentPane().add(lblNewLabel_5);
+		
+		textField_2 = new JTextField();
+		textField_2.setBackground(new Color(0, 0, 0));
+		textField_2.setForeground(new Color(0, 0, 0));
+		textField_2.setOpaque(false); // transparent
+		textField_2.setFont(new Font("SansSerif", Font.PLAIN, 13));
+		textField_2.setBorder(bottomBorder);
+		textField_2.setBounds(470, 241, 40, 25);
+		textField_2.setColumns(5);
+		frame.getContentPane().add(textField_2);
+		//addValidationToTextField(textField2);
+		
+		
+		JLabel lblNewLabel_6 = new JLabel("Number of team members");
+		lblNewLabel_6.setFont(new Font("SansSerif", Font.PLAIN, 13));
+		lblNewLabel_6.setBounds(219, 280, 151, 18);
+		frame.getContentPane().add(lblNewLabel_6);
+		
+		textField_3 = new JTextField();
+		textField_3.setBackground(new Color(0, 0, 0));
+		textField_3.setForeground(new Color(0, 0, 0));
+		textField_3.setOpaque(false); // transparent
+		textField_3.setFont(new Font("SansSerif", Font.PLAIN, 13));
+		textField_3.setBorder(bottomBorder);
+		textField_3.setBounds(470, 275, 40, 25);
+		textField_3.setColumns(5);
+		frame.getContentPane().add(textField_3);
+		
+		JLabel lblNewLabel_7 = new JLabel("Sprint Velocity");
+		lblNewLabel_7.setFont(new Font("SansSerif", Font.PLAIN, 13));
+		lblNewLabel_7.setBounds(219, 314, 151, 18);
+		frame.getContentPane().add(lblNewLabel_7);
+		
+		textField_4 = new JTextField();
+		textField_4.setBackground(new Color(0, 0, 0));
+		textField_4.setForeground(new Color(0, 0, 0));
+		textField_4.setOpaque(false); // transparent
+		textField_4.setFont(new Font("SansSerif", Font.PLAIN, 13));
+		textField_4.setBorder(bottomBorder);
+		textField_4.setBounds(470, 309, 40, 25);
+		textField_4.setColumns(5);
+		frame.getContentPane().add(textField_4);
+		
+		JLabel lblNewLabel_8 = new JLabel("Sprint Duration");
+		lblNewLabel_8.setFont(new Font("SansSerif", Font.PLAIN, 13));
+		lblNewLabel_8.setBounds(219, 347, 151, 18);
+		frame.getContentPane().add(lblNewLabel_8);
+		
+		textField_5 = new JTextField();
+		textField_5.setBackground(new Color(0, 0, 0));
+		textField_5.setForeground(new Color(0, 0, 0));
+		textField_5.setOpaque(false);
+		textField_5.setFont(new Font("SansSerif", Font.PLAIN, 13));
+		textField_5.setBorder(bottomBorder);
+		textField_5.setBounds(470, 340, 40, 25);
+		textField_5.setColumns(5);
+		frame.getContentPane().add(textField_5);
+		
+        JLabel lblNewLabel_9 = new JLabel("Choose a development scenario");
+        lblNewLabel_9.setFont(new Font("SansSerif", Font.PLAIN, 13));
+        lblNewLabel_9.setBounds(117, 426, 200, 18);
+        frame.getContentPane().add(lblNewLabel_9);
+        
+        JComboBox<String> comboBox = new JComboBox();
+        comboBox.setUI(new NoBorderComboBoxUI());
+        comboBox.setUI(new BasicComboBoxUI());
+        comboBox.setBounds(353, 426, 300, 25);
+        frame.getContentPane().add(comboBox);
+        
+        comboBox.addItem("Tea Game");
+        comboBox.addItem("Coffee Game");
+        comboBox.addItem("Pizza Game");
+        
+        comboBox.setFont(new Font("SansSerif", Font.PLAIN, 13));
+        comboBox.setBackground(Color.decode("#f0ebd8"));
+        comboBox.setForeground(Color.BLACK);
+        comboBox.setFocusable(false);
+
+        comboBox.setBorder(bottomBorder);
+        
+        comboBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
+        
+		JLabel lblNewLabel_10 = new JLabel("or");
+		lblNewLabel_10.setFont(new Font("SansSerif", Font.PLAIN, 13));
+		lblNewLabel_10.setBounds(364, 458, 13, 17);
+		frame.getContentPane().add(lblNewLabel_10);
+		
+		RoundButton btnNewButton_3 = new RoundButton("Add Custom Stories");
+		btnNewButton_3.setForeground(Color.WHITE);
+		btnNewButton_3.setFont(new Font("SansSerif", Font.PLAIN, 13));
+		btnNewButton_3.setBackground(Color.decode("#3e5c76"));
+		btnNewButton_3.setFocusPainted(false);
+		btnNewButton_3.setBorderPainted(false);
+		btnNewButton_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_3.setBounds(293, 490, 162, 45);
+		frame.getContentPane().add(btnNewButton_3);
+		
+		RoundButton btnNewButton_4 = new RoundButton("Continue");
+		btnNewButton_4.setForeground(Color.WHITE);
+		btnNewButton_4.setFont(new Font("SansSerif", Font.PLAIN, 13));
+		btnNewButton_4.setBackground(Color.decode("#026e0d"));
+		btnNewButton_4.setFocusPainted(false);
+		btnNewButton_4.setBorderPainted(false);
+		btnNewButton_4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frame.dispose();
+				
+		        SwingUtilities.invokeLater(new Runnable() {
+		            public void run() {
+		            	SimulationInProgressPage SimulationInProgressPage = new SimulationInProgressPage();
+		                SimulationInProgressPage.setVisible(true);
+		            }
+		        });
+			}
+		});
+				
+		btnNewButton_4.setBounds(303, 630, 142, 40);
+		frame.getContentPane().add(btnNewButton_4);
+                
+        JPanel panel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.setColor(Color.decode("#797979"));
+                int x1 = 253;
+                int y1 = 396;
+                int x2 = 453;
+                int y2 = 396;
+
+                Graphics2D g2d = (Graphics2D) g;
+                g2d.setStroke(new BasicStroke(1));
+                g2d.drawLine(x1, y1, x2, y2);
+                
+                int x3 = 266;
+                int y3 = 567;
+                int x4 = 466;
+                int y4 = 567;
+                g2d.drawLine(x3, y3, x4, y4);
+            }
+        };
+        panel.setBounds(0, 0, frame.getWidth(), frame.getHeight());
+        panel.setBackground(Color.decode("#f0ebd8"));
+        frame.getContentPane().add(panel);
+        panel.setLayout(new SpringLayout());
+
+    }
+	
+	/*
+	public void addValidationToTextField(JTextField textField) {
+	    textField.getDocument().addDocumentListener(new DocumentListener() {
+	        @Override
+	        public void insertUpdate(DocumentEvent e) {
+	            validateInput(textField);
+	        }
+
+	        @Override
+	        public void removeUpdate(DocumentEvent e) {
+	            validateInput(textField);
+	        }
+
+	        @Override
+	        public void changedUpdate(DocumentEvent e) {
+	            validateInput(textField);
+	        }
+	    });
+	}
+
+	public void validateInput(JTextField textField) {
+	    String text = textField.getText();
+	    try {
+	        int number = Integer.parseInt(text);
+	        if (number < 0 || number > 999) {
+	            textField.setText("");
+	        }
+	    } catch (NumberFormatException ex) {
+	        textField.setText("");
+	    }
+	}
+}
+*/
+
+class RoundButton extends JButton {
+    public RoundButton(String label) {
+        super(label);
+        setContentAreaFilled(false);
+        setFocusPainted(false);
+        setBorderPainted(false);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        if (getModel().isArmed()) {
+            g.setColor(Color.gray);
+        } else {
+            g.setColor(getBackground());
+        }
+        g.fillRoundRect(0, 0, getWidth(), getHeight(), 10, 10);
+        super.paintComponent(g);
+    }
+
+    @Override
+    protected void paintBorder(Graphics g) {
+        g.setColor(getForeground());
+        g.drawRoundRect(0, 0, getWidth(), getHeight(), 10, 10);
+    }
+
+    protected Shape getShape() {
+        RoundRectangle2D shape = new RoundRectangle2D.Float(0, 0, getWidth(), getHeight(), 10, 10);
+        return shape;
+    }
+
+    @Override
+    public boolean contains(int x, int y) {
+        if (getShape() == null || !getShape().contains(x, y)) {
+            return false;
+        }
+        return super.contains(x, y);
+    }
+}
+
+class NoBorderComboBoxUI extends BasicComboBoxUI {
+    @Override
+    protected ComboPopup createPopup() {
+        BasicComboPopup popup = new BasicComboPopup(comboBox) {
+            @Override
+            protected JScrollPane createScroller() {
+                return new JScrollPane(list, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+            }
+        };
+        popup.getAccessibleContext().setAccessibleParent(comboBox);
+        return popup;
+    }
+}
+}
---
+++ AgileMentor/src/SimulationInProgressPage.java
@@ -0,0 +1,188 @@
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+import javax.swing.SpringLayout;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+import javax.swing.ImageIcon;
+
+public class SimulationInProgressPage extends JFrame {
+	
+	private static final long serialVersionUID = 1L;
+	private JFrame frame;
+	
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					SimulationInProgressPage frame = new SimulationInProgressPage();
+					frame.setVisible(false);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public SimulationInProgressPage() {
+		initialize();
+	}
+	
+	private void initialize()
+	{
+		Create_A_Simulation frontendcalltoCAS = new Create_A_Simulation();
+		String no_of_sprints = frontendcalltoCAS.getTextField_2().getText();
+		System.out.println(no_of_sprints);
+		
+		
+		frame = new JFrame();
+		frame.setVisible(true);
+		frame.setBounds(350, 120, 754, 719); // Size of frame
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		frame.getContentPane().setBackground(Color.decode("#f0ebd8"));
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+		
+		JButton signOutButton = new JButton("Sign Out");
+		signOutButton.setForeground(Color.RED);
+		signOutButton.setFont(new Font("SansSerif", Font.PLAIN, 13));
+		signOutButton.setOpaque(false); // Transparent color
+		signOutButton.setContentAreaFilled(false); 
+		signOutButton.setBorderPainted(false); // Hide border
+		signOutButton.setFocusPainted(false);
+		signOutButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) { // Need to make link between LoginPage
+				frame.dispose();
+				
+				LoginPage LoginPage = new LoginPage(); // New Frame (NewScreen)
+				LoginPage.setVisible(true);
+				
+			}
+		});
+		frame.getContentPane().setLayout(null);
+		signOutButton.setBounds(40, 22, 120, 40);
+		frame.getContentPane().add(signOutButton);
+		
+		JLabel lblNewLabel = new JLabel("Simulation in Progress");
+		lblNewLabel.setFont(new Font("SansSerif", Font.BOLD, 25));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(221, 16, 305, 43);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JButton saveButton = new JButton("");
+		saveButton.setIcon(new ImageIcon("C:\\Users\\Rushabh\\Downloads\\icons8-save-material-rounded\\icons8-save-48.png"));
+		saveButton.setForeground(Color.RED);
+		saveButton.setFont(new Font("SansSerif", Font.PLAIN, 13));
+		saveButton.setOpaque(true); // Transparent color
+		saveButton.setContentAreaFilled(false); 
+		saveButton.setBorderPainted(false); // Hide border
+		saveButton.setFocusPainted(false);
+		saveButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) { // Need to make link between LoginPage
+				
+				
+			}
+		});
+		saveButton.setBounds(536, 22, 76, 50);
+		frame.getContentPane().add(saveButton);
+		
+		JButton homeButton = new JButton("");
+		homeButton.setIcon(new ImageIcon("C:\\Users\\Rushabh\\Downloads\\icons8-home-ios-16-filled\\icons8-home-50.png"));
+		homeButton.setForeground(Color.RED);
+		homeButton.setFont(new Font("SansSerif", Font.PLAIN, 13));
+		homeButton.setOpaque(true); // Transparent color
+		homeButton.setContentAreaFilled(false); 
+		homeButton.setBorderPainted(false); // Hide border
+		homeButton.setFocusPainted(false);
+		homeButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) { // Need to make link between LoginPage
+				
+				
+			}
+		});
+		homeButton.setBounds(608, 22, 70, 50);
+		frame.getContentPane().add(homeButton);
+		
+		JProgressBar progressBar = new JProgressBar();
+		progressBar.setBounds(144, 119, 438, 28);
+		progressBar.setForeground(new Color(2, 110, 13));
+		progressBar.setBackground(new Color(255, 255, 255));
+		progressBar.setValue(0);
+		progressBar.setStringPainted(true);
+		frame.getContentPane().add(progressBar);
+		
+		JButton playButton = new JButton("");
+		playButton.setIcon(new ImageIcon("C:\\Users\\Rushabh\\Downloads\\icons8-play-50.png"));
+		playButton.setForeground(Color.RED);
+		playButton.setFont(new Font("SansSerif", Font.PLAIN, 13));
+		playButton.setOpaque(true); // Transparent color
+		playButton.setContentAreaFilled(false); 
+		playButton.setBorderPainted(false); // Hide border
+		playButton.setFocusPainted(false);
+		playButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) { // Need to make link between LoginPage
+				
+				
+			}
+		});
+		playButton.setBounds(279, 158, 70, 50);
+		frame.getContentPane().add(playButton);
+		
+		JButton rewindButton = new JButton("");
+		rewindButton.setIcon(new ImageIcon("C:\\Users\\Rushabh\\Downloads\\icons8-rewind-material-sharp\\icons8-rewind-48.png"));
+		rewindButton.setForeground(Color.RED);
+		rewindButton.setFont(new Font("SansSerif", Font.PLAIN, 13));
+		rewindButton.setOpaque(true); // Transparent color
+		rewindButton.setContentAreaFilled(false); 
+		rewindButton.setBorderPainted(false); // Hide border
+		rewindButton.setFocusPainted(false);
+		rewindButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) { // Need to make link between LoginPage
+				
+				
+			}
+		});
+		rewindButton.setBounds(359, 158, 70, 50);
+		frame.getContentPane().add(rewindButton);
+		
+		JButton randomEventsButton = new JButton("Add Random Event");
+		randomEventsButton.setBackground(new Color(62, 92, 118));
+		randomEventsButton.setIcon(null);
+		randomEventsButton.setForeground(Color.decode("#f0ebd8"));
+		randomEventsButton.setFont(new Font("SansSerif", Font.PLAIN, 15));
+		randomEventsButton.setOpaque(true); // Transparent color
+		randomEventsButton.setContentAreaFilled(true); 
+		randomEventsButton.setBorderPainted(false); // Hide border
+		randomEventsButton.setFocusPainted(false);
+		randomEventsButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) { // Need to make link between LoginPage
+				
+				
+			}
+		});
+		randomEventsButton.setBounds(506, 158, 224, 50);
+		frame.getContentPane().add(randomEventsButton);
+		
+		
+		
+		
+		
+
+	}
+}
