0d3c73d68371252d8bc984a097f5dde93a0ac08e
sverma89 <sverma89@asu.edu>
2023-11-07 15:25:42 -0700

    added business value and story point attribute to the sprint class

--- AgileMentor/src/scrum_sim_packages/Sprint.java
+++ AgileMentor/src/scrum_sim_packages/Sprint.java
@@ -3,6 +3,43 @@ package AgileMentor.src.scrum_sim_packages;
 import java.util.ArrayList;
 
 public class Sprint {
+    private long expectedBV;
+    private double expectedSP;
+    private long actualBV;
+    private double actualSP;
+
+    public long getExpectedBV() {
+        return expectedBV;
+    }
+
+    public void setExpectedBV(long expectedBV) {
+        this.expectedBV = expectedBV;
+    }
+
+    public double getExpectedSP() {
+        return expectedSP;
+    }
+
+    public void setExpectedSP(double expectedSP) {
+        this.expectedSP = expectedSP;
+    }
+
+    public long getActualBV() {
+        return actualBV;
+    }
+
+    public void setActualBV(long actualBV) {
+        this.actualBV = actualBV;
+    }
+
+    public double getActualSP() {
+        return actualSP;
+    }
+
+    public void setActualSP(double actualSP) {
+        this.actualSP = actualSP;
+    }
+
     private Backlog productBacklog;
     private Backlog sprintBacklog;
     private ArrayList<StandupDay> standupDays;
