24d56c291aaf671019b3a3f77bc73aed08e40cf8
sverma89 <sverma89@asu.edu>
2023-11-09 17:55:40 -0700

    simulation engine done

--- AgileMentor/src/SimulationInProgressBackEnd.java
+++ AgileMentor/src/SimulationInProgressBackEnd.java
@@ -1,14 +1,13 @@
 package AgileMentor.src;
 
 
-import AgileMentor.src.scrum_sim_packages.SimulationSession;
-import AgileMentor.src.scrum_sim_packages.Sprint;
-import AgileMentor.src.scrum_sim_packages.UserStory;
-import AgileMentor.src.scrum_sim_packages.UserStoryModel;
+import AgileMentor.src.scrum_sim_packages.*;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import static java.lang.Long.parseLong;
+import static java.lang.Math.min;
 
 public class SimulationInProgressBackEnd
 {
@@ -24,12 +23,15 @@ public class SimulationInProgressBackEnd
 		
 	}
 
-	static ArrayList<UserStory> removeSprintStoriesFromProductBacklog(ArrayList<UserStory>productBacklog, ArrayList<UserStory>sprintBacklog){
+	static void removeSprintStoriesFromProductBacklog(ArrayList<UserStory>productBacklog, ArrayList<UserStory>sprintBacklog){
 		int itr = 0;
 		boolean isChanged = false;
 		while(itr < productBacklog.size()){
 			isChanged = false;
 			for(UserStory us: sprintBacklog) {
+				if(itr >= productBacklog.size()){
+					break;
+				}
 				if (productBacklog.get(itr).getId() == us.getId()) {
 					productBacklog.remove(itr);
 					isChanged = true;
@@ -39,11 +41,10 @@ public class SimulationInProgressBackEnd
 				itr++;
 			}
 		}
-		return productBacklog;
 	}
 	// Prints the items which are put
 	// in a knapsack of capacity W
-	static ArrayList<UserStory> printknapSack(int W, ArrayList<UserStory> pb, int n)
+	static ArrayList<UserStory> printknapSack(int W, ArrayList<UserStory> pb, int n, Sprint currentSprint)
 	{
 		int i, w;
 		int K[][] = new int[n + 1][W + 1];
@@ -63,6 +64,7 @@ public class SimulationInProgressBackEnd
 
 		// stores the result of Knapsack
 		int res = K[n][W];
+		currentSprint.setExpectedBV(res);
 		ArrayList<UserStory> sprintBacklog = new ArrayList<>();
 		w = W;
 		for (i = n; i > 0 && res > 0; i--) {
@@ -71,8 +73,8 @@ public class SimulationInProgressBackEnd
 			else {
 
 				// This item is included.
-				System.out.println(pb.get(i-1).getName());
 				sprintBacklog.add(pb.get(i-1));
+				currentSprint.setExpectedSP(currentSprint.getExpectedSP()+pb.get(i-1).getStoryPoints());
 
 				// Since this weight is included its
 				// value is deducted
@@ -82,48 +84,75 @@ public class SimulationInProgressBackEnd
 		}
 		return sprintBacklog;
 	}
-	public ArrayList<String> calcSimulationParameters(String sessionName,String noOfsprints,String noOfteamMembers,String sprintVelocity,String sprintDuration)
+	public SimulationSession calcSimulationParameters(String sessionName,String noOfsprints,String noOfteamMembers,String sprintVelocity,String sprintDuration)
 	{
+		Random rndm = new Random();
 		ArrayList<Sprint> sprints = new ArrayList<>();
 		ArrayList<UserStory> productBacklog = UserStoryModel.getUserStories();
-		SimulationSession simulationSession = new SimulationSession(sessionName, parseLong(sprintVelocity), parseLong(sprintDuration), parseLong(noOfsprints), parseLong(noOfteamMembers), sprints);
+		ArrayList<InformationCard> informationCards = InformationCardModel.getInformationCards();
+		SimulationSession simulationSession = new SimulationSession(sessionName, parseLong(sprintVelocity), parseLong(sprintDuration), parseLong(noOfsprints), parseLong(noOfteamMembers), sprints, "user1");
 		for(int i = 0; i < parseLong(noOfsprints); i++){
-			ArrayList<UserStory> sprintBacklog = printknapSack((int) simulationSession.getVelocity(), productBacklog, productBacklog.size());
-			productBacklog = removeSprintStoriesFromProductBacklog(productBacklog, sprintBacklog);
-			for(UserStory us: productBacklog){
-				System.out.println(us.getName());
+			Sprint currentSprint = new Sprint(0,0,0,0,null, null, null);
+			ArrayList<UserStory> sprintBacklog = printknapSack((int) simulationSession.getVelocity(), productBacklog, productBacklog.size(), currentSprint);
+			removeSprintStoriesFromProductBacklog(productBacklog, sprintBacklog);
+			currentSprint.setProductBacklog(new Backlog(productBacklog));
+			currentSprint.setSprintBacklog(new Backlog(sprintBacklog));
+			long numStories = sprintBacklog.size();
+			long numEmployees = simulationSession.getTeamSize();
+			long freeEmployees = numEmployees - min(numEmployees, numStories);
+			long currentNumberOfStories = min(numEmployees, numStories);
+			ArrayList<StandupDay> standupDays = new ArrayList<>();
+			for (int j = 0; j < simulationSession.getSprintDuration(); j++){
+				StandupDay standupDay = new StandupDay(j+1, null);
+				ArrayList<StandupStoryProgress> standupStoryProgresses = new ArrayList<>();
+				int k = 0;
+				while (k < min(currentNumberOfStories, sprintBacklog.size())){
+					InformationCard currentCard = informationCards.get(rndm.nextInt(informationCards.size()));
+					StandupStoryProgress currentStandupStoryProgress = new StandupStoryProgress(sprintBacklog.get(k), currentCard, "Good Job", sprintBacklog.get(k).getStoryPoints(), sprintBacklog.get(k).getBusinessValue());
+					if(currentCard.getType().equals("Progress")){
+						currentSprint.setActualSP(currentSprint.getActualSP()+sprintBacklog.get(k).getStoryPoints()*currentCard.getProgress()/100.00);
+						sprintBacklog.get(k).setStoryPoints(sprintBacklog.get(k).getStoryPoints()*(1-currentCard.getProgress()/100.00));
+						if(currentCard.getProgress() == 100.0){
+							currentSprint.setActualBV(currentSprint.getActualBV()+sprintBacklog.get(k).getBusinessValue());
+							sprintBacklog.get(k).setBusinessValue(0);
+							currentStandupStoryProgress.setRemainingBusinessValue(0);
+							currentStandupStoryProgress.setRemainingStoryPoints(sprintBacklog.get(k).getStoryPoints());
+							sprintBacklog.remove(k);
+							freeEmployees++;
+						}
+						else{
+							currentStandupStoryProgress.setRemainingStoryPoints(sprintBacklog.get(k).getStoryPoints());
+							k++;
+						}
+					}
+					else{
+						if(freeEmployees > 0){
+							currentStandupStoryProgress.setResponse("Resolved");
+							freeEmployees--;
+							k++;
+						}
+						else{
+							currentStandupStoryProgress.setResponse("Blocked");
+							sprintBacklog.add(sprintBacklog.get(k));
+							sprintBacklog.remove(k);
+						}
+					}
+					standupStoryProgresses.add(currentStandupStoryProgress);
+					System.out.println(currentStandupStoryProgress.getUserStory().getName());
+					System.out.println(currentStandupStoryProgress.getRemainingStoryPoints());
+				}
+				standupDay.setStandupStoryProgresses(standupStoryProgresses);
+				standupDays.add(standupDay);
+			}
+			currentSprint.setStandupDays(standupDays);
+			sprints.add(currentSprint);
+			ArrayList<UserStory> userStories = UserStoryModel.getUserStories();
+			for (UserStory us: sprintBacklog){
+				productBacklog.add(userStories.get((int) (us.getId()-1)));
 			}
 		}
-
-//		ArrayList<UserStory> sprintBacklog = printknapSack(simulationSession.getVelocity(), productBacklog, productBacklog.size());
-
-		String sessionNameBE = sessionName;
-		String noOfsprintsBE = noOfsprints;
-		String noOfteamMembersBE = noOfteamMembers;
-		String sprintVelocityBE = sprintVelocity;
-		String sprintDurationBE = sprintDuration;
-		ArrayList<String> sprintParametersBE = new ArrayList<String>();
-		sprintParametersBE.add(sessionName);
-		sprintParametersBE.add(noOfsprintsBE);
-		sprintParametersBE.add(noOfteamMembersBE);
-		sprintParametersBE.add(sprintVelocityBE);
-		sprintParametersBE.add(sprintDurationBE);
-		return sprintParametersBE ;
-		
-		
+		simulationSession.setSprints(sprints);
+		return simulationSession;
 	}
-	
-	//SimulationInProgressPage frontendcalltoSIPP = new SimulationInProgressPage();
-//	System.out.println("In SIPBE");
-//	String sessionName = frontendcalltoSIPP.sessionName;
-//	System.out.println(sessionName);
-//	String noOfsprints = frontendcalltoSIPP.noOfsprints;
-//	System.out.println(noOfsprints);
-//	String noOfteamMembers = frontendcalltoSIPP.noOfteamMembers;
-//	System.out.println(noOfteamMembers);
-//	String sprintVelocity = frontendcalltoSIPP.sprintVelocity;
-//	System.out.println(sprintVelocity);
-//	String sprintDuration = frontendcalltoSIPP.sprintDuration;
-//	System.out.println(sprintDuration);
 
 }
