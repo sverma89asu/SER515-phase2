f1459d91000222343c65eac5ca893650d536c6ba
sverma89 <sverma89@asu.edu>
2023-11-15 18:59:13 -0700

    resolved comments

--- AgileMentor/src/SimulationInProgressBackEnd.java
+++ AgileMentor/src/SimulationInProgressBackEnd.java
@@ -25,7 +25,7 @@ public class SimulationInProgressBackEnd
 	}
 	// Prints the items which are put
 	// in a knapsack of capacity W
-	static ArrayList<UserStory> printknapSack(int W, ArrayList<UserStory> pb, int n)
+	static ArrayList<UserStory> printknapSack(int W, ArrayList<UserStory> productBacklog, int n)
 	{
 		int i, w;
 		int K[][] = new int[n + 1][W + 1];
@@ -35,9 +35,9 @@ public class SimulationInProgressBackEnd
 			for (w = 0; w <= W; w++) {
 				if (i == 0 || w == 0)
 					K[i][w] = 0;
-				else if (pb.get(i-1).getStoryPoints() <= w)
-					K[i][w] = (int) Math.max(pb.get(i-1).getBusinessValue() +
-							K[i - 1][w - (int)pb.get(i-1).getStoryPoints()], K[i - 1][w]);
+				else if (productBacklog.get(i-1).getStoryPoints() <= w)
+					K[i][w] = (int) Math.max(productBacklog.get(i-1).getBusinessValue() +
+							K[i - 1][w - (int)productBacklog.get(i-1).getStoryPoints()], K[i - 1][w]);
 				else
 					K[i][w] = K[i - 1][w];
 			}
@@ -53,13 +53,13 @@ public class SimulationInProgressBackEnd
 			else {
 
 				// This item is included.
-				System.out.println(pb.get(i-1).getDescription());
-				sprintBacklog.add(pb.get(i-1));
+				System.out.println(productBacklog.get(i-1).getDescription());
+				sprintBacklog.add(productBacklog.get(i-1));
 
 				// Since this weight is included its
 				// value is deducted
-				res = (int) (res - pb.get(i-1).getBusinessValue());
-				w = (int) (w - pb.get(i-1).getStoryPoints());
+				res = (int) (res - productBacklog.get(i-1).getBusinessValue());
+				w = (int) (w - productBacklog.get(i-1).getStoryPoints());
 			}
 		}
 		return sprintBacklog;
@@ -68,7 +68,7 @@ public class SimulationInProgressBackEnd
 	{
 		ArrayList<Sprint> sprints = new ArrayList<>();
 		ArrayList<UserStory> productBacklog = UserStoryModel.getUserStories();
-		SimulationSession simulationSession = new SimulationSession(sessionName, parseLong(sprintVelocity), parseLong(sprintDuration), parseLong(noOfsprints), parseLong(noOfteamMembers), sprints);
+		SimulationSession simulationSession = new SimulationSession(sessionName, parseLong(sprintVelocity), parseLong(sprintDuration), parseLong(noOfsprints), parseLong(noOfteamMembers), sprints, "user1");
 		for(int i = 0; i < parseLong(noOfsprints); i++){
 			ArrayList<UserStory> sprintBacklog = printknapSack((int) simulationSession.getVelocity(), productBacklog, productBacklog.size());
 
