5d37656c153cf0ed65d975cd8f90f52094ab23e4
vbhurke <vbhurke@asu.edu>
2023-11-24 01:42:03 +0530

    Function created to Parse JSON file in Java Object

---
+++ AgileSimulator/src/main/java/Helpers/ImportJSON.java
@@ -0,0 +1,29 @@
+package Helpers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import scrum_sim_packages.LoginPage;
+import scrum_sim_packages.SaveButtonReaction;
+import scrum_sim_packages.SimulationSession;
+import java.io.File;
+
+import java.io.IOException;
+public class ImportJSON {
+    public static SimulationSession LoadSession(String fileName){
+        ObjectMapper objectMapper = new ObjectMapper();
+        SimulationSession sim = null;
+        try {
+            // Replace "path/to/your/file.json" with the actual path to your JSON file
+            File file = new File(fileName);
+
+            // Read JSON file and convert to Java object
+            sim = objectMapper.readValue(file, SimulationSession.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return sim;
+    }
+}
