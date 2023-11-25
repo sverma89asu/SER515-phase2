aafd8a6b408cee136be9823267b9d055cbe8efae
spark290 <spark290@asu.edu>
2023-11-25 16:14:20 -0700

    Ready for Test

--- AgileSimulator/src/main/java/scrum_sim_packages/CreateAccount.java
+++ AgileSimulator/src/main/java/scrum_sim_packages/CreateAccount.java
@@ -13,6 +13,7 @@ public class CreateAccount extends JFrame {
     private JTextField username_textField;
     private JPasswordField passwordField;
     private JPasswordField passwordVerificationField;
+    private JTextField email_textField;
 
     public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
@@ -25,6 +26,12 @@ public class CreateAccount extends JFrame {
                 }
             }
         });
+    }
+
+    private boolean isValidEmail (String email){
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+        return email.matches(emailRegex);
+
     }
 
     /**
@@ -47,17 +54,17 @@ public class CreateAccount extends JFrame {
         JLabel createAccount = new JLabel("Create Account");
         createAccount.setFont(new Font("SansSerif", Font.BOLD, 32));
         createAccount.setHorizontalAlignment(SwingConstants.CENTER);
-        createAccount.setBounds(150, 85, 450, 50);
+        createAccount.setBounds(150, 45, 450, 50);
         frame.getContentPane().add(createAccount);
 
         JLabel username = new JLabel("Username");
         username.setFont(new Font("SansSerif", Font.PLAIN, 20));
         //lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        username.setBounds(150, 160, 200, 40);
+        username.setBounds(150, 120, 200, 40);
         frame.getContentPane().add(username);
 
         username_textField = new JTextField();
-        username_textField.setBounds(150, 210, 450, 40);
+        username_textField.setBounds(150, 170, 450, 40);
         username_textField.setBorder(new LineBorder(new Color(171, 173, 179), 1, true));
         username_textField.setMaximumSize(new Dimension(7, 19));
         frame.getContentPane().add(username_textField);
@@ -65,36 +72,48 @@ public class CreateAccount extends JFrame {
 
         JLabel password = new JLabel("Password");
         password.setFont(new Font("SansSerif", Font.PLAIN, 20));
-        password.setBounds(150, 270, 200, 40);
+        password.setBounds(150, 230, 200, 40);
         frame.getContentPane().add(password);
 
         passwordField = new JPasswordField();
-        passwordField.setBounds(150, 320, 450, 40);
+        passwordField.setBounds(150, 280, 450, 40);
         passwordField.setBorder(new LineBorder(new Color(171, 173, 179), 1, true));
         frame.getContentPane().add(passwordField);
 
         JLabel passwordVerification = new JLabel("Password Verification");
         passwordVerification.setFont(new Font("SansSerif", Font.PLAIN, 20));
-        passwordVerification.setBounds(150, 380, 200, 40);
+        passwordVerification.setBounds(150, 340, 200, 40);
         frame.getContentPane().add(passwordVerification);
 
         passwordVerificationField = new JPasswordField();
-        passwordVerificationField.setBounds(150, 430, 450, 40);
+        passwordVerificationField.setBounds(150, 390, 450, 40);
         passwordVerificationField.setBorder(new LineBorder(new Color(171, 173, 179), 1, true));
         frame.getContentPane().add(passwordVerificationField);
 
         JLabel selectRole = new JLabel("Select Role");
         selectRole.setFont(new Font("SansSerif", Font.PLAIN, 20));
-        selectRole.setBounds(150, 490, 200, 40);
+        selectRole.setBounds(150, 450, 200, 40);
         frame.getContentPane().add(selectRole);
 
         JComboBox<String> roleComboBox = new JComboBox<>(new String[]{"Trainee", "Trainer", "Sim User"});
         roleComboBox.setFont(new Font("SansSerif", Font.PLAIN, 20));
-        roleComboBox.setBounds(150, 540, 450, 40);
+        roleComboBox.setBounds(150, 500, 450, 40);
         frame.getContentPane().add(roleComboBox);
 
+        JLabel email = new JLabel("E-mail");
+        email.setFont(new Font("SansSerif", Font.PLAIN, 20));
+        email.setBounds(150, 560, 200, 40);
+        frame.getContentPane().add(email);
+
+        email_textField = new JTextField();
+        email_textField.setBounds(150, 610, 450, 40);
+        email_textField.setBorder(new LineBorder(new Color(171, 173, 179), 1, true));
+        email_textField.setMaximumSize(new Dimension(7, 19));
+        frame.getContentPane().add(email_textField);
+        email_textField.setColumns(10);
+
         JButton create = new JButton("Create");
-        create.setBounds(300, 600, 150, 43);
+        create.setBounds(300, 670, 150, 43);
         create.setFont(new Font("SansSerif", Font.BOLD, 20));
         create.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
         create.setBackground(Color.decode("#3e5c76"));
@@ -109,11 +128,14 @@ public class CreateAccount extends JFrame {
                 String username = username_textField.getText();
                 String password = String.valueOf(passwordField.getPassword());
                 String passwordVerification = String.valueOf(passwordVerificationField.getPassword());
+                String email = email_textField.getText();
 
-                if (username.isEmpty() || password.isEmpty() || passwordVerification.isEmpty()) {
+                if (username.isEmpty() || password.isEmpty() || passwordVerification.isEmpty() || email.isEmpty() ) {
                     JOptionPane.showMessageDialog(frame, "Check Your Input");
                 } else if (!password.equals(passwordVerification)) {
                     JOptionPane.showMessageDialog(frame, "Check Password Verification");
+                } else if (!isValidEmail(email)) {  // Email validation check
+                    JOptionPane.showMessageDialog(frame, "Check Your Email Address");
                 } else {
                     // If password is identical, the account should be created
                 }
