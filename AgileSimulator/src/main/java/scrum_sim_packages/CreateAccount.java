9849b475421b30b50dcb4536d9cfa927ecd13941
spark290 <spark290@asu.edu>
2023-11-25 17:06:01 -0700

    Ready for Test

--- AgileSimulator/src/main/java/scrum_sim_packages/CreateAccount.java
+++ AgileSimulator/src/main/java/scrum_sim_packages/CreateAccount.java
@@ -45,7 +45,7 @@ public class CreateAccount extends JFrame {
     {
         frame = new JFrame();
         frame.setVisible(true);
-        frame.setBounds(350, 120, 754, 800);
+        frame.setBounds(350, 120, 754, 750);
         frame.getContentPane().setLayout(null);
         frame.getContentPane().setBackground(Color.decode("#f0ebd8"));
         frame.setResizable(false);
@@ -54,17 +54,17 @@ public class CreateAccount extends JFrame {
         JLabel createAccount = new JLabel("Create Account");
         createAccount.setFont(new Font("SansSerif", Font.BOLD, 32));
         createAccount.setHorizontalAlignment(SwingConstants.CENTER);
-        createAccount.setBounds(150, 45, 450, 50);
+        createAccount.setBounds(150, 25, 450, 50);
         frame.getContentPane().add(createAccount);
 
         JLabel username = new JLabel("Username");
         username.setFont(new Font("SansSerif", Font.PLAIN, 20));
         //lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        username.setBounds(150, 120, 200, 40);
+        username.setBounds(150, 100, 200, 40);
         frame.getContentPane().add(username);
 
         username_textField = new JTextField();
-        username_textField.setBounds(150, 170, 450, 40);
+        username_textField.setBounds(150, 150, 450, 40);
         username_textField.setBorder(new LineBorder(new Color(171, 173, 179), 1, true));
         username_textField.setMaximumSize(new Dimension(7, 19));
         frame.getContentPane().add(username_textField);
@@ -72,48 +72,48 @@ public class CreateAccount extends JFrame {
 
         JLabel password = new JLabel("Password");
         password.setFont(new Font("SansSerif", Font.PLAIN, 20));
-        password.setBounds(150, 230, 200, 40);
+        password.setBounds(150, 210, 200, 40);
         frame.getContentPane().add(password);
 
         passwordField = new JPasswordField();
-        passwordField.setBounds(150, 280, 450, 40);
+        passwordField.setBounds(150, 260, 450, 40);
         passwordField.setBorder(new LineBorder(new Color(171, 173, 179), 1, true));
         frame.getContentPane().add(passwordField);
 
         JLabel passwordVerification = new JLabel("Password Verification");
         passwordVerification.setFont(new Font("SansSerif", Font.PLAIN, 20));
-        passwordVerification.setBounds(150, 340, 200, 40);
+        passwordVerification.setBounds(150, 320, 200, 40);
         frame.getContentPane().add(passwordVerification);
 
         passwordVerificationField = new JPasswordField();
-        passwordVerificationField.setBounds(150, 390, 450, 40);
+        passwordVerificationField.setBounds(150, 370, 450, 40);
         passwordVerificationField.setBorder(new LineBorder(new Color(171, 173, 179), 1, true));
         frame.getContentPane().add(passwordVerificationField);
 
         JLabel selectRole = new JLabel("Select Role");
         selectRole.setFont(new Font("SansSerif", Font.PLAIN, 20));
-        selectRole.setBounds(150, 450, 200, 40);
+        selectRole.setBounds(150, 430, 200, 40);
         frame.getContentPane().add(selectRole);
 
         JComboBox<String> roleComboBox = new JComboBox<>(new String[]{"Trainee", "Trainer", "Sim User", "Admin"});
         roleComboBox.setFont(new Font("SansSerif", Font.PLAIN, 20));
-        roleComboBox.setBounds(150, 500, 450, 40);
+        roleComboBox.setBounds(150, 480, 450, 40);
         frame.getContentPane().add(roleComboBox);
 
         JLabel email = new JLabel("E-mail");
         email.setFont(new Font("SansSerif", Font.PLAIN, 20));
-        email.setBounds(150, 560, 200, 40);
+        email.setBounds(150, 540, 200, 40);
         frame.getContentPane().add(email);
 
         email_textField = new JTextField();
-        email_textField.setBounds(150, 610, 450, 40);
+        email_textField.setBounds(150, 590, 450, 40);
         email_textField.setBorder(new LineBorder(new Color(171, 173, 179), 1, true));
         email_textField.setMaximumSize(new Dimension(7, 19));
         frame.getContentPane().add(email_textField);
         email_textField.setColumns(10);
 
         JButton create = new JButton("Create");
-        create.setBounds(300, 670, 150, 43);
+        create.setBounds(300, 650, 150, 43);
         create.setFont(new Font("SansSerif", Font.BOLD, 20));
         create.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
         create.setBackground(Color.decode("#3e5c76"));
